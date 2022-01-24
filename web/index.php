b723b4b38103c9c51296c44c8b8e51211cf3f0d1
emiliort <emiliort@gmail.com>
2022-01-25 00:45:41 +0100

    remove vlucas/phpdotenv dependence

--- web/index.php
+++ web/index.php
@@ -2,12 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * Load application environment from .env file
- */
-$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
-$dotenv->load();
-
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
