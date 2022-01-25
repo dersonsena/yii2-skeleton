6ee9edf9f7552608c6b367bc8e5df96c839fe5ae
emiliort <emiliort@gmail.com>
2022-01-25 01:44:57 +0100

    Clean up comments

--- web/index.php
+++ web/index.php
@@ -1,10 +1,9 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
